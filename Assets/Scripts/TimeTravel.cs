61354f6624ab8e2987c281b8282f0779cfbcccef
HasanRazaRawjani <rawjanihasan@gmail.com>
2025-05-17 11:20:46 -0400

    a

--- Assets/Scripts/TimeTravel.cs
+++ Assets/Scripts/TimeTravel.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TimeTravel : MonoBehaviour
 {
     public GameObject PlayerSpawn;
+    public GameObject fpscam;
+    private GameObject effect1;
+    private GameObject effect2;
 
-    // Update is called once per frame
+    public float shakeDuration = 0.2f;
+    public float shakeMagnitude = 0.2f;
+    private Coroutine shakeCoroutine;
+
+    void Awake()
+    {
+        effect1 = PlayerSpawn.transform.Find("Effect1").gameObject;
+        effect2 = PlayerSpawn.transform.Find("Effect2").gameObject;
+        effect1.SetActive(false);
+        effect2.SetActive(false);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
@@ -19,6 +34,33 @@ public class TimeTravel : MonoBehaviour
     {
         gameObject.transform.position = PlayerSpawn.transform.position; 
         gameObject.transform.rotation = PlayerSpawn.transform.rotation; 
+
+        if (shakeCoroutine != null)
+            StopCoroutine(shakeCoroutine);
+        shakeCoroutine = StartCoroutine(ScreenShake());
     }
 
+    IEnumerator ScreenShake()
+    {
+        Vector3 originalPos = fpscam.transform.localPosition;
+        float elapsed = 0f;
+
+        while (elapsed < shakeDuration)
+        {
+            effect1.SetActive(true);
+            effect2.SetActive(true);
+            float x = Random.Range(-1f, 1f) * shakeMagnitude;
+            float y = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            fpscam.transform.localPosition = originalPos + new Vector3(x, y, 0);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        effect1.SetActive(false);
+        effect2.SetActive(false);
+
+        fpscam.transform.localPosition = originalPos;
+    }
 }
